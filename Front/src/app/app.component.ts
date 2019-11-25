d41cf72def21b40a8fe910825e57a203a792d939
Soulwolfos <s.soulwolfos@gmail.com>
2019-11-25 05:10:08 -0300

    se puueden ver lo que escribe el cliente y dentro de la caja del chat

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('cuerpoTarjeta', {static: true})
+  private cuerpoTarjeta: ElementRef;
+
+  constructor(private renderer: Renderer2){
+  }
 
 
+  mensajeCliente = '';
 
+
+  enviarMensaje(){
+
+    const pe = this.renderer.createElement('p');
+    const texto = this.renderer.createText("Yo: " + this.mensajeCliente);
+    this.renderer.appendChild(pe, texto);
+    this.renderer.appendChild(this.cuerpoTarjeta.nativeElement, pe);
+    pe.scrollIntoView();
+
+    this.mensajeCliente = '';
+
+  }
 }
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
