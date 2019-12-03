4ed538d27d8adb4c9cfa84c9212d22f449e0e621
Soulwolfos <s.soulwolfos@gmail.com>
2019-12-03 00:14:27 -0300

    se ecribe en el chat la respuesta del bot

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -3,6 +3,11 @@ import { connectableObservableDescriptor } from 'rxjs/internal/observable/Connec
 import { HttpClient } from '@angular/common/http';
 import { post } from 'selenium-webdriver/http';
 
+interface myData{
+  obj: Object
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,6 +26,8 @@ export class AppComponent {
 
   textoObj = Object.create({});
 
+
+
   enviarMensaje(){
 
     const pe = this.renderer.createElement('p');
@@ -38,12 +45,15 @@ export class AppComponent {
 
   }
 
-
+  //'http://127.0.0.1:8000/API/chatbot'
   onEnviarPost(postData: { text: string} ){
-    this.http.post('http://127.0.0.1:8000/API/chatbot.json', postData)
+    this.http.post('https://backchatbottest.firebaseio.com/chatbot.json', postData) //cambiar a la url del chatbot, esa url es de prueba
       .subscribe(responseData => {
-        console.log(responseData);
-        //aqui deberia escribir la respuesta en el chat (creo)
+        const pe = this.renderer.createElement('p');
+        const texto = this.renderer.createText('Bot: ' + responseData.name);//en el chart bot deberia ser .text , el .name es para probar
+        this.renderer.appendChild(pe, texto);
+        this.renderer.appendChild(this.cuerpoTarjeta.nativeElement, pe);
+        pe.scrollIntoView();
     });
 
   }
